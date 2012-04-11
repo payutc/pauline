1d6d79dc0d464a043dde809e86a31ad4ada57ecf
Thomas Recouvreux <thomas.recouvreux@gmail.com>
2012-04-11 19:43:33 +0200

    commit oublié

---
+++ src/fr/utc/assos/payutc/BaseActivity.java
@@ -0,0 +1,78 @@
+package fr.utc.assos.payutc;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+public class BaseActivity extends NfcActivity {
+	private static final String LOG_TAG		= "BaseActivity";
+	
+	protected PaulineSession mSession;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+        mSession = PaulineSession.load(getIntent());
+	}
+	
+	@Override
+	protected void onSaveInstanceState(Bundle b) {
+		mSession.save(b);
+	}
+	
+	@Override 
+	final public void startActivityForResult(Intent intent, int requestCode) {
+		mSession.save(intent);
+		super.startActivityForResult(intent, requestCode);
+	}
+	
+	@Override 
+	final public void startActivity(Intent intent) {
+		mSession.save(intent);
+		super.startActivity(intent);
+	}
+	
+	@Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (resultCode!=RESULT_CANCELED) {
+			mSession = PaulineSession.load(data);
+		}
+	}
+
+	@Override
+	protected void onIdentification(String id) {
+		Log.d(LOG_TAG, "Identified : "+id);
+	}
+	
+	/**
+	 * Finir l'activité courante, si l'activité ne s'est pas finie avec succès,
+	 * on ne sauvegarde pas l'état courant de la session.
+	 * @param success
+	 */
+	final protected void stop(Boolean success) {
+		if (success) {
+			stop(RESULT_OK);
+		}
+		else {
+			stop(RESULT_CANCELED);
+		}
+	}
+	
+	/**
+	 * Finir l'activité courante
+	 * @param returnCode si == RESULT_CANCEL, la session n'est pas sauvegardée
+	 */
+	final protected void stop(int returnCode) {
+		Intent intent = new Intent();
+		if (returnCode != RESULT_CANCELED) {
+			mSession.save(intent);
+		}
+        setResult(RESULT_OK, intent);
+		finish();
+	}
+	
+	final protected void stop() {
+		stop(RESULT_OK);
+	}
+}
