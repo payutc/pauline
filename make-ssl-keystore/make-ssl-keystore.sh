e58321d93f446d0950525d95ccdbff8001dda351
Thomas Recouvreux <thomas.recouvreux@gmail.com>
2012-06-20 11:58:23 +0200

    make-ssl-keystore tool

---
+++ make-ssl-keystore/make-ssl-keystore.sh
@@ -0,0 +1,16 @@
+export CLASSPATH=bcprov-jdk15on-146.jar
+CERTSTORE=res/raw/mystore.bks
+if [ -a $CERTSTORE ]; then
+    rm $CERTSTORE || exit 1
+fi
+keytool \
+      -import \
+      -v \
+      -trustcacerts \
+      -alias 0 \
+      -file <(openssl x509 -in certificat.pem) \
+      -keystore $CERTSTORE \
+      -storetype BKS \
+      -provider org.bouncycastle.jce.provider.BouncyCastleProvider \
+      -providerpath /usr/share/java/bcprov.jar \
+      -storepass pauline
